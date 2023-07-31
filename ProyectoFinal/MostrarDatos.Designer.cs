f72e5c60a5573062b7435378fde44c160b56e4f7
HenrrySL1 <140290730+HenrrySL1@users.noreply.github.com>
2023-07-31 13:17:00 -0500

    Cambio realizado

--- ProyectoFinal/MostrarDatos.Designer.cs
+++ ProyectoFinal/MostrarDatos.Designer.cs
@@ -37,20 +37,20 @@ namespace ProyectoFinal
             // dgvDatosAlmacenados
             // 
             this.dgvDatosAlmacenados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvDatosAlmacenados.Location = new System.Drawing.Point(7, 129);
-            this.dgvDatosAlmacenados.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvDatosAlmacenados.Location = new System.Drawing.Point(9, 159);
+            this.dgvDatosAlmacenados.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvDatosAlmacenados.Name = "dgvDatosAlmacenados";
             this.dgvDatosAlmacenados.RowHeadersWidth = 51;
             this.dgvDatosAlmacenados.RowTemplate.Height = 24;
-            this.dgvDatosAlmacenados.Size = new System.Drawing.Size(582, 226);
+            this.dgvDatosAlmacenados.Size = new System.Drawing.Size(776, 278);
             this.dgvDatosAlmacenados.TabIndex = 0;
             // 
             // btnMostrar
             // 
-            this.btnMostrar.Location = new System.Drawing.Point(226, 35);
-            this.btnMostrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnMostrar.Location = new System.Drawing.Point(301, 43);
+            this.btnMostrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnMostrar.Name = "btnMostrar";
-            this.btnMostrar.Size = new System.Drawing.Size(130, 57);
+            this.btnMostrar.Size = new System.Drawing.Size(173, 70);
             this.btnMostrar.TabIndex = 1;
             this.btnMostrar.Text = "Mostrar Datos";
             this.btnMostrar.UseVisualStyleBackColor = true;
@@ -58,13 +58,13 @@ namespace ProyectoFinal
             // 
             // MostrarDatos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnMostrar);
             this.Controls.Add(this.dgvDatosAlmacenados);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MostrarDatos";
             this.Text = "MostrarDatos";
             ((System.ComponentModel.ISupportInitialize)(this.dgvDatosAlmacenados)).EndInit();
